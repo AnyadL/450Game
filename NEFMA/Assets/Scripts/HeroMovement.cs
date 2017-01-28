dfb05a30c3885340b260e5a9bd29eb36ef470baa
lexie bartlett <lexie@ualberta.ca>
2017-01-28 15:35:13 -0700

    Some platforming working, joystick movement, jump broken

---
+++ NEFMA/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float movespeed;
+    public float jumpForce = 8f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO: Make it so hero can't jump off the screen if they continuously click button 
+        grounded = true;
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+          
+           moveDirection.y = jumpForce;
+
+        }
+        else
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
+            rb.AddForce(movement * movespeed);
+        }
+
+    }
+	
+}
