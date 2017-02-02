422c54d0d95d9079759fe93356eb00dfe3bff3ba
lexie bartlett <lexie@ualberta.ca>
2017-02-01 22:39:49 -0700

    test hero can move and jump with controller, working on enemy collison you can attack when you are within range but it doesn't do anything yet just prints attack in the log

--- NEFMA/Assets/Scripts/HeroMovement.cs
+++ NEFMA/Assets/Scripts/HeroMovement.cs
@@ -4,38 +4,91 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour {
 
-    public Rigidbody2D rb;
-    public float movespeed;
-    public float jumpForce = 8f;
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+    [HideInInspector] public bool attack = false;
+    [HideInInspector]public bool inRange = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
     public Transform groundCheck;
-    public string playerNumber;
 
     private bool grounded = false;
-    private Vector3 moveDirection = Vector3.zero;
-
+    //private Animator anim;
+    private Rigidbody2D rb2d;
 
     // Use this for initialization
-    void Start () {
-        rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //TODO: Make it so hero can't jump off the screen if they continuously click button 
-        grounded = true;
-        if (Input.GetButtonDown("Jump_" + playerNumber) && grounded)
-        {
-          
-           moveDirection.y = jumpForce;
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
-        }
-        else
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump_1") && grounded)
         {
-            float moveHorizontal = Input.GetAxis("Horizontal_" + playerNumber);
-            Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
-            rb.AddForce(movement * movespeed);
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            attack = true;
         }
 
     }
-	
+
+    //Does the actions during the frame the hero has to do, in this case jumping, flipping and attacking
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal_1");
+
+        //anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            //anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            jump = false;
+        }
+        if (attack && inRange)
+        {
+            Debug.Log("attack");
+            attack = false;
+        }
+    }
+
+    //Check if within hitting range of an enemy, enemies should have 2 colliders
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            inRange = true;
+        }
+    }
+
+    //flip the sprite in different directions when switching
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
