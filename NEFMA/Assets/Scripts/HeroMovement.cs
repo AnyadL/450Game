cfcbdfb758cb1f92999c712ddb16a09bc87146f6
Dylan Cassidy <the.dylan.cassidy@gmail.com>
2017-02-01 18:50:48 -0700

    added multiple controller support

--- NEFMA/Assets/Scripts/HeroMovement.cs
+++ NEFMA/Assets/Scripts/HeroMovement.cs
@@ -8,6 +8,7 @@ public class HeroMovement : MonoBehaviour {
     public float movespeed;
     public float jumpForce = 8f;
     public Transform groundCheck;
+    public string playerNumber;
 
     private bool grounded = false;
     private Vector3 moveDirection = Vector3.zero;
@@ -22,7 +23,7 @@ public class HeroMovement : MonoBehaviour {
 	void Update () {
         //TODO: Make it so hero can't jump off the screen if they continuously click button 
         grounded = true;
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Jump_" + playerNumber) && grounded)
         {
           
            moveDirection.y = jumpForce;
@@ -30,7 +31,7 @@ public class HeroMovement : MonoBehaviour {
         }
         else
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveHorizontal = Input.GetAxis("Horizontal_" + playerNumber);
             Vector2 movement = new Vector2(moveHorizontal, rb.velocity.y);
             rb.AddForce(movement * movespeed);
         }
