86176c452489cdd9899e13c8829540936ae37fc7
lexie bartlett <lexie@ualberta.ca>
2017-02-07 12:21:09 -0700

    You can shoot enemies now with Agni

---
+++ NEFMA/Assets/Scripts/AttributeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributeController : MonoBehaviour {
+
+    public float health;
+    
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        health = health - 1; 
+    }
+}
