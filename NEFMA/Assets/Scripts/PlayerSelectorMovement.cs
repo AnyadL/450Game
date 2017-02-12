af5189d0107bb5aa48301b40d8e14c3ca0011860
DaynaLacoursiere <daynalacoursiere@gmail.com>
2017-02-12 01:31:27 -0700

    made new rooms that don't have watermarks and such... Added thumbtacks to the posters. Added a P2->P4 selectors

--- NEFMA/Assets/Scripts/PlayerSelectorMovement.cs
+++ NEFMA/Assets/Scripts/PlayerSelectorMovement.cs
@@ -7,84 +7,105 @@ using UnityEngine.SceneManagement;
 public class PlayerSelectorMovement : MonoBehaviour {
     
     Vector3 position;
-    int topX = 270;
-    int bottomX = 320;
+    int topX = 400;
+    int bottomX = 600;
     int topY = 70;
     int bottomY = -50;
+    public int player;
 
     // Use this for initialization
     void Start () {
-        position = new Vector3(-topX, topY, 0);
+        switch(player)
+        {
+            case 0:
+                position = new Vector3(-topX, topY, 0);
+                break;
+            case 1:
+                position = new Vector3(0, topY, 0);
+                break;
+
+            case 2:
+                position = new Vector3(topX, topY, 0);
+                break;
+
+            case 3:
+                position = new Vector3(-bottomX, bottomY, 0);
+                break;
+        }
         GetComponent<RectTransform>().localPosition = position;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("space"))
-        {
-            SceneManager.LoadScene(2);
-        }
-        if  (Input.GetKeyDown("up"))
-        {
-            if(!(GetComponent<RectTransform>().localPosition.y == topY))
-            {
-                position.y = topY;
-                if (position.x == bottomX)
-                    position.x = topX;
-                else if (position.x == -bottomX)
-                    position.x = -topX;
-                  
-                GetComponent<RectTransform>().localPosition = position;
-            }
-        }
-        else if (Input.GetKeyDown("down"))
-        {
-            if (!(GetComponent<RectTransform>().localPosition.y == bottomY))
-            {
-                position.y = bottomY;
-                if (position.x == topX)
-                    position.x = bottomX;
-                else if (position.x == -topX)
-                    position.x = -bottomX;
 
-                GetComponent<RectTransform>().localPosition = position;
-            }
-        }
-        else if (Input.GetKeyDown("left"))
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == 0)
         {
-            if ((GetComponent<RectTransform>().localPosition.y == topY))
+            if (Input.GetKeyDown("space"))
             {
-                if (!(GetComponent<RectTransform>().localPosition.x == -topX))
+                SceneManager.LoadScene(2);
+            }
+            if (Input.GetKeyDown("up"))
+            {
+                if (!(GetComponent<RectTransform>().localPosition.y == topY))
                 {
-                    position.x = GetComponent<RectTransform>().localPosition.x - topX;
+                    position.y = topY;
+                    if (position.x == bottomX)
+                        position.x = topX;
+                    else if (position.x == -bottomX)
+                        position.x = -topX;
+
                     GetComponent<RectTransform>().localPosition = position;
                 }
             }
-            else
+            else if (Input.GetKeyDown("down"))
             {
-                if (!(GetComponent<RectTransform>().localPosition.x == -bottomX))
+                if (!(GetComponent<RectTransform>().localPosition.y == bottomY))
                 {
-                    position.x = GetComponent<RectTransform>().localPosition.x - bottomX;
+                    position.y = bottomY;
+                    if (position.x == topX)
+                        position.x = bottomX;
+                    else if (position.x == -topX)
+                        position.x = -bottomX;
+
                     GetComponent<RectTransform>().localPosition = position;
                 }
             }
-        }
-        else if (Input.GetKeyDown("right"))
-        {
-            if ((GetComponent<RectTransform>().localPosition.y == topY))
+            else if (Input.GetKeyDown("left"))
             {
-                if (!(GetComponent<RectTransform>().localPosition.x == topX))
+                if ((GetComponent<RectTransform>().localPosition.y == topY))
                 {
-                    position.x = GetComponent<RectTransform>().localPosition.x + topX;
-                    GetComponent<RectTransform>().localPosition = position;
+                    if (!(GetComponent<RectTransform>().localPosition.x == -topX))
+                    {
+                        position.x = GetComponent<RectTransform>().localPosition.x - topX;
+                        GetComponent<RectTransform>().localPosition = position;
+                    }
+                }
+                else
+                {
+                    if (!(GetComponent<RectTransform>().localPosition.x == -bottomX))
+                    {
+                        position.x = GetComponent<RectTransform>().localPosition.x - bottomX;
+                        GetComponent<RectTransform>().localPosition = position;
+                    }
                 }
             }
-            else
+            else if (Input.GetKeyDown("right"))
             {
-                if (!(GetComponent<RectTransform>().localPosition.x == bottomX))
+                if ((GetComponent<RectTransform>().localPosition.y == topY))
                 {
-                    position.x = GetComponent<RectTransform>().localPosition.x + bottomX;
-                    GetComponent<RectTransform>().localPosition = position;
+                    if (!(GetComponent<RectTransform>().localPosition.x == topX))
+                    {
+                        position.x = GetComponent<RectTransform>().localPosition.x + topX;
+                        GetComponent<RectTransform>().localPosition = position;
+                    }
+                }
+                else
+                {
+                    if (!(GetComponent<RectTransform>().localPosition.x == bottomX))
+                    {
+                        position.x = GetComponent<RectTransform>().localPosition.x + bottomX;
+                        GetComponent<RectTransform>().localPosition = position;
+                    }
                 }
             }
         }
