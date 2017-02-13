f92cff446390a8e98300e579a2c1be40ad8db919
DaynaLacoursiere <daynalacoursiere@gmail.com>
2017-02-13 16:44:47 -0700

    posters in hero selector change appropriately when you move around. Need to add multiplayer and actual selection next

--- NEFMA/Assets/Scripts/PlayerSelectorMovement.cs
+++ NEFMA/Assets/Scripts/PlayerSelectorMovement.cs
@@ -11,11 +11,37 @@ public class PlayerSelectorMovement : MonoBehaviour {
     int bottomX = 600;
     int topY = 70;
     int bottomY = -50;
-    public int player;
+    public int playerNumber;
+    public Text playerText;
+    public Image playerPanel;
+    public Sprite delilahPoster;
+    public Sprite agniPoster;
+    public Sprite rykerPoster;
+    public Sprite kittyPoster;
+    public Sprite blankPoster;
+    Vector3 delilahPosition;
+    Vector3 agniPosition;
+    Vector3 rykerPosition;
+    Vector3 kittyPosition;
+    string delilahText;
+    string agniText;
+    string rykerText;
+    string kittyText;
+
 
     // Use this for initialization
     void Start () {
-        switch(player)
+        delilahPosition = new Vector3(-topX, topY, 0);
+        agniPosition = new Vector3(topX, topY, 0);
+        rykerPosition = new Vector3(0, topY, 0);
+        kittyPosition = new Vector3(-bottomX, bottomY, 0);
+
+        delilahText = "I may be shy, but don’t let that fool you!  I can summon giant energy hands to protect people.  I can even use my shields to send foes flying!  Is it any wonder I'm the leader of the school’s anti-bullying club?";
+        agniText = "I'm a real firebrand, blessed with the super power to manipulate flames. Unfortunately, I'm not so hot at controlling my powers, and my wardrobe has gone up in flames more often than I can count.";
+        rykerText = "I'm a very good runner. I can run past my enemies so quickly I completely disorient them.  Someday I'm going to be a supersonic hero.";
+        kittyText = "I am the queen of the school improv club. I'm a  social butterfly, and my feline agility makes me the acrobat of the team. In battle, I am “furocious”, favoring a barrage of quick strikes and huge leaps.";
+
+        switch (playerNumber)
         {
             case 0:
                 position = new Vector3(-topX, topY, 0);
@@ -38,7 +64,7 @@ public class PlayerSelectorMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (player == 0)
+        if (playerNumber == 0)
         {
             if (Input.GetKeyDown("space"))
             {
@@ -109,8 +135,36 @@ public class PlayerSelectorMovement : MonoBehaviour {
                 }
             }
         }
+        setPlayerPanel();
+    }
+
+    void setPlayerPanel()
+    {
+        if (position == rykerPosition)
+        {
+            playerPanel.sprite = rykerPoster;
+            playerText.text = rykerText;
+        }
+        else if (position == delilahPosition)
+        {
+            playerPanel.sprite = delilahPoster;
+            playerText.text = delilahText;
+        }
+        else if (position == agniPosition)
+        {
+            playerPanel.sprite = agniPoster;
+            playerText.text = agniText;
+        }
+        else if (position == kittyPosition)
+        {
+            playerPanel.sprite = kittyPoster;
+            playerText.text = kittyText;
+        }
+        else
+        {
+            playerPanel.sprite = blankPoster;
+            playerText.text = null;
+        }
     }
 }
 
-// (-270,70) (0,70) (270,70)
-// (-320,-50) (0,-50) (320,-50)
