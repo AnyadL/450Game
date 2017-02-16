e7dc66115a052efeff243f6d10631c4dc0e67c2f
DaynaLacoursiere <daynalacoursiere@gmail.com>
2017-02-15 21:10:09 -0700

    Added in menu music and sound effects. Also got the options working (so you can change the volume)

---
+++ NEFMA/Assets/Scripts/onSliderChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class onSliderChange : MonoBehaviour {
+
+    public Slider slider;
+    public bool music;
+    private AudioSource[] sounds;
+
+    void Start ()
+    {
+        sounds = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+    }
+
+    public void changeVolume ()
+    {
+        for(int i = 0; i < sounds.Length; ++i)
+        {
+            // Check for untagged sounds
+            if ((sounds[i].tag != "SoundFX") && (sounds[i].tag != "Music"))
+            {
+                print("There is an untagged sound. Please tag your sounds as Music or SoundFX. Sound:" + sounds[i].name);
+            }
+
+            // If this is the music slider, change music volume
+            if (music && (sounds[i].tag == "Music"))
+            {
+                sounds[i].volume = slider.value;
+            }
+            // If this is the sound effects slider, change sound effects volume
+            else if (!music && (sounds[i].tag == "SoundFX"))
+            {
+                sounds[i].volume = slider.value;
+            }
+        }
+    }
+}
