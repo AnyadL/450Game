afbb3526be429ef96ff0ab9c65c936f7cafab51a
Michael Morris <mcmorris@ualberta.ca>
2017-02-27 10:17:23 -0700

    Add files via upload
    Uploaded base classes for dialogue system (Conversation - data, DialoguePane - UI, ConversationRunner - controller).  Needs expansion of ConversationRunner for handling Conversations in-game, and needs exploration of input of dialogue - ie. from XML?

---
+++ NEFMA/Assets/Scripts/ConversationRunner.cs
@@ -0,0 +1,42 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: NEFMA
+ * File: ConversationRunner.cs
+ * 
+ * Description: ConversationRunner actually runs a conversation, and tracks
+ * the current position of the dialogue in the conversation (ie. node 8).
+ * Voiceover progress needs to be tracked here as well.
+ * 
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationRunner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadConversation(Conversation newConversation) {
+        conversation = newConversation;
+        currentIndex = 0;
+    }
+
+    void setPane(DialoguePane newWindow) {
+        window = newWindow;
+    }
+
+
+    public Conversation conversation;   // FIXME: Public only for testing, must be private for release!
+    public DialoguePane window;         // FIXME: Public only for testing, must be private for release!
+    public int currentIndex;            // FIXME: Public only for testing, must be private for release!
+    
+
+}
---
+++ NEFMA/Assets/Scripts/DialoguePane.cs
@@ -0,0 +1,21 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: NEFMA
+ * File: DialoguePane.cs
+ * 
+ * Description: DialoguePane which reads through data in Conversation instances.
+ * Should fade in and out and only update when a Conversation is active.
+ * 
+ * Credits: 
+ * Rough structure based on the one suggested by Tony Li here:
+ * https://forum.unity3d.com/threads/dialogue-system-tutorial.315618/
+ * 
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialoguePane : MonoBehaviour {
+    public UnityEngine.UI.Text dialogueText;
+    // TODO: Timer variable before advancing to next text.
+}
