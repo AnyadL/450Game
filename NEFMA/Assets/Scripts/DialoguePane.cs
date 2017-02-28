96052631d2a226e70141f1f632740f69810b8133
Michael Morris <mcmorris@ualberta.ca>
2017-02-27 18:18:02 -0700

    Update DialoguePane.cs
    DialoguePane structure extended to include basic needed functions.

--- NEFMA/Assets/Scripts/DialoguePane.cs
+++ NEFMA/Assets/Scripts/DialoguePane.cs
@@ -1,4 +1,4 @@
-﻿/******************************************************************************
+/******************************************************************************
  * Author: Michael Morris
  * Course: NEFMA
  * File: DialoguePane.cs
@@ -16,6 +16,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialoguePane : MonoBehaviour {
+
+    void open() {
+        // TODO: Open dialogue pane here.
+    }
+
+    void loadText(string text) {
+        dialogueText.text = text;
+        // TODO: Set dirty flag to force redraw here.
+    }
+
+    public void close() {
+        // TODO: Close dialogue pane here.
+    }
+
     public UnityEngine.UI.Text dialogueText;
     // TODO: Timer variable before advancing to next text.
 }
