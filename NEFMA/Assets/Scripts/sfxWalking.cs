be653a1ef7095d0b915d5394b0b98b9f0dee69ce
Justin Daza <justinjo@ualberta.ca>
2017-03-05 17:33:43 -0700

    added Walking sound fx script and sound files

---
+++ NEFMA/Assets/Scripts/sfxWalking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfxWalking : MonoBehaviour {
+    HeroMovement hm;
+	// Use this for initialization
+	void Start () {
+        hm = GetComponent<HeroMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(hm.grounded == true && hm.currentMaxSpeed > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
