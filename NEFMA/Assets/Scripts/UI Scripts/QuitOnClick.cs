16f801c3ba2b5258490aee2c38f5e94a12396240
DaynaLacoursiere <daynalacoursiere@gmail.com>
2017-03-17 17:54:16 -0600

    pause menu quit button returns to the main menu instead of quitting the game

--- NEFMA/Assets/Scripts/UI Scripts/QuitOnClick.cs
+++ NEFMA/Assets/Scripts/UI Scripts/QuitOnClick.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class QuitOnClick : MonoBehaviour
 {
@@ -11,5 +12,15 @@ public class QuitOnClick : MonoBehaviour
 #else
         Application.Quit ();
 #endif
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Globals.gamePaused = false;
+        Time.timeScale = 1;
+        Globals.players.Clear();
+        Globals.numPlayers = 0;
+        Globals.livingPlayers = 0;
+        SceneManager.LoadScene(0);
     }
 }
