98c0009fe181cee400fdba6596bb50e0ccfda91e
lexie bartlett <lexie@ualberta.ca>
2017-03-17 19:50:24 -0600

    Kittys claws move with you, size increase, they go in the direction you turn.

--- NEFMA/Assets/Scripts/ClawScript.cs
+++ NEFMA/Assets/Scripts/ClawScript.cs
@@ -6,9 +6,20 @@ public class ClawScript : MonoBehaviour {
 
     public float attackTime = 1.5f;
     // Use this for initialization
+    [HideInInspector] public GameObject owner;
+    [HideInInspector]public float velocityDirection;
+
     void Start()
     {
+        transform.position = owner.transform.position + new Vector3(6 * velocityDirection, 1, 0);
         StartCoroutine(AttackTime());
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = owner.transform.position + new Vector3(6*velocityDirection, 1, 0);
+        
     }
     IEnumerator AttackTime()
     {
