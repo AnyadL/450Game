8529d67c6bde18c3ad832db5f38ba303d54195e4
DaynaLacoursiere <daynalacoursiere@gmail.com>
2017-03-22 22:19:44 -0600

    All cutscenes mostly done now (tiny issue in No Monster Abuse)

--- NEFMA/Assets/Scripts/DialogueController.cs
+++ NEFMA/Assets/Scripts/DialogueController.cs
@@ -15,6 +15,10 @@ public class DialogueController : MonoBehaviour {
 
     public Sprite sittingMilo;
 
+    public GameObject mrFluffy;
+
+    public GameObject[] monsterList;
+
     public Texture2D fadeTexture;
 
     private GameObject bubble;
@@ -37,7 +41,9 @@ public class DialogueController : MonoBehaviour {
     private bool drGtime = false;
     private float drGmove = 0f;
     private bool monsterTime = false;
+    private bool monstersMoving = false;
     private bool miloMoved = false;
+    private float[] schreiHeights = new float[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     
     private Vector3 tempTransform;
     
@@ -58,6 +64,17 @@ public class DialogueController : MonoBehaviour {
                 createBubble(orderedNames[i], ellipsisPrefab);
 
             }
+        }
+
+        int j = 0;
+        for (int i = 0; i < monsterList.Length; ++i)
+        {
+            if (monsterList[i].name == "Schrei")
+            {
+                schreiHeights[j] = monsterList[i].transform.position.y;
+                ++j;
+            }
+
         }
     }
 
@@ -361,10 +378,62 @@ public class DialogueController : MonoBehaviour {
 
                 drGtime = true;
                 break;
+
             case 2:
                 if (timePause == 0)
                 {
+                    timePause = 1f;
+
+                    updateTime = Time.time + timePause;
+                }
+                else if (timePause == 1f && Time.time >= updateTime)
+                {
+                    timePause = 2f;
+
+                    updateTime = Time.time + timePause;
+
+                    mrFluffy.GetComponent<HeroMovement>().moveCharacter(1, 2450);
+                }
+                else if (Time.time >= updateTime)
+                {
+                    mrFluffy.GetComponent<Rigidbody2D>().isKinematic = true;
+                    mrFluffy.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+                    mrFluffy.GetComponent<Rigidbody2D>().isKinematic = false;
+
+
+                    currentSpecial = 0;
+                    timePause = 0;
+                }
+
+                break;
+            case 3:
+                if (timePause == 0)
+                {
                     timePause = 3f;
+
+                    updateTime = Time.time + timePause;
+                }
+                else if (timePause == 3f && Time.time >= updateTime)
+                {
+                    drGmove = -0.3f;
+                    findPlayerObject("Milo").transform.FindChild("EllipsisBubble(Clone)").gameObject.SetActive(false);
+                    mrFluffy.GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Milo").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+
+                    timePause = 5.5f;
+
+                    updateTime = Time.time + timePause;
+                    
+                }
+                else if (timePause == 5.5f && Time.time >= updateTime)
+                {
+                    
+                    findPlayerObject("Ryker").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Agni").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Kitty").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Delilah").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+
+                    timePause = 4f;
 
                     updateTime = Time.time + timePause;
                 }
@@ -379,7 +448,7 @@ public class DialogueController : MonoBehaviour {
         }
         if (drGtime)
         {
-            if (drG.transform.position.x <= 45f)
+            if (drG.transform.position.x <= 45f && drGmove != -0.3f)
                 drGmove = 0;
             drG.transform.position = new Vector3(drG.transform.position.x + drGmove, 12f + (Mathf.Sin(2 * Time.time) * 2), drG.transform.position.z);
         }
@@ -414,8 +483,7 @@ public class DialogueController : MonoBehaviour {
                 }
                 else if (Time.time >= updateTime)
                 {
-
-                    // stop kitty's slide
+                    
                     findPlayerObject("Ryker").GetComponent<Rigidbody2D>().isKinematic = true;
                     findPlayerObject("Ryker").GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
                     findPlayerObject("Ryker").GetComponent<Rigidbody2D>().isKinematic = false;
@@ -463,27 +531,111 @@ public class DialogueController : MonoBehaviour {
 
                 drGtime = true;
                 break;
+
             case 2:
+                if (timePause == 0)
+                {
+                    timePause = 1f;
+
+                    updateTime = Time.time + timePause;
+                }
+                else if (timePause == 1f && Time.time >= updateTime)
+                {
+                    timePause = 2f;
+
+                    updateTime = Time.time + timePause;
+
+                    mrFluffy.GetComponent<HeroMovement>().moveCharacter(1, 2450);
+                }
+                else if (Time.time >= updateTime)
+                {
+                    mrFluffy.GetComponent<Rigidbody2D>().isKinematic = true;
+                    mrFluffy.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+                    mrFluffy.GetComponent<Rigidbody2D>().isKinematic = false;
+
+
+                    currentSpecial = 0;
+                    timePause = 0;
+                }
+
+                break;
+            case 3:
                 if (timePause == 0)
                 {
                     timePause = 3f;
 
                     updateTime = Time.time + timePause;
                 }
-                else if (Time.time >= updateTime)
+                else if (timePause == 3f && Time.time >= updateTime)
+                {
+                    drGmove = -0.3f;
+                    findPlayerObject("Milo").transform.FindChild("ExclamationBubble(Clone)").gameObject.SetActive(false);
+                    mrFluffy.GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Milo").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+
+                    timePause = 5.5f;
+
+                    updateTime = Time.time + timePause;
+
+                }
+                else if (timePause == 5.5f && Time.time >= updateTime)
+                {
+
+                    findPlayerObject("Ryker").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Agni").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Kitty").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    findPlayerObject("Delilah").GetComponent<HeroMovement>().moveCharacter(-1, 600f);
+                    
+
+                    monsterTime = true;
+                    monstersMoving = false;
+                }
+                break;
+            case 4:
+                if (timePause == 0)
+                {
+                    
+                    timePause = 3f;
+
+                    updateTime = Time.time + timePause;
+                }
+                else if (timePause == 3f && Time.time >= updateTime)
                 {
                     // fade out
                     Globals.gamePaused = false;
                     Globals.fading = true;
                     Globals.fadeDir = 1;
+
                 }
                 break;
         }
         if (drGtime)
         {
-            if (drG.transform.position.x <= 45f)
+            if (drG.transform.position.x <= 45f && drGmove != -0.3f)
                 drGmove = 0;
             drG.transform.position = new Vector3(drG.transform.position.x + drGmove, 12f + (Mathf.Sin(2 * Time.time) * 2), drG.transform.position.z);
+        }
+        if (monsterTime)
+        {
+            int j = 0;
+            for (int i = 0; i < monsterList.Length; ++i)
+            {
+                if (monsterList[i].name == "Loafer" && monsterList[i].GetComponent<Rigidbody2D>().velocity.x == 0f)
+                {
+                    monsterList[i].GetComponent<HeroMovement>().moveCharacter(-1, 800f);
+                    monstersMoving = true;
+                }
+                else if (monsterList[i].name == "Soaker" && monsterList[i].transform.position.y < -9f)
+                {
+                    monsterList[i].GetComponent<HeroMovement>().jumpCharacter(Random.Range(-30f, -50f), 100f);
+                }
+                else if (monsterList[i].name == "Schrei")
+                {
+                    monsterList[i].transform.position = new Vector3(monsterList[i].transform.position.x - 0.3f, schreiHeights[j] + (Mathf.Sin(Random.Range(1,2) * Time.time) * 2), monsterList[i].transform.position.z);
+                    ++j;
+                }
+                
+            }
         }
     }
 }
