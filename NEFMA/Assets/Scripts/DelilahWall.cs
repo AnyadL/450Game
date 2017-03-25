e62984283fa03c35f73dd4bbebbaf28da9cb471b
lexie bartlett <lexie@ualberta.ca>
2017-03-25 15:54:17 -0600

    Delilah's wall can be pushed away, does not explode yet

--- NEFMA/Assets/Scripts/DelilahWall.cs
+++ NEFMA/Assets/Scripts/DelilahWall.cs
@@ -9,6 +9,7 @@ public class DelilahWall : MonoBehaviour {
     [HideInInspector] public bool free = false;
     public float shieldTime;
     public int health = 30;
+   
 
     // Use this for initialization
     void Start () {
@@ -30,6 +31,15 @@ public class DelilahWall : MonoBehaviour {
         if (!free)
         {
             transform.position = owner.transform.position + new Vector3(4 * wallRight, 1.8f, 0);
+        }
+        else{
+            Debug.Log("free");
+            float velocityDirection = owner.GetComponent<DelilahAttack>().wallVelocity;
+            if (!owner.GetComponent<HeroMovement>().facingRight)
+            {
+                velocityDirection = -velocityDirection;
+            }
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(velocityDirection, 0);
         }
     }
 
